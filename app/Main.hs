01b67bd68005589803248baff18387c8ea523ee3
Jade Piramide <jadepiramidepogi@gmail.com>
2023-02-04 23:35:57 -0800

    Create basic Todo data type that holds all tasks

--- app/Main.hs
+++ app/Main.hs
@@ -1,6 +1,7 @@
 module Main (main) where
 
 import Lib
+import Todo
 
 main :: IO ()
 main = someFunc
--- src/Todo.hs
+++ src/Todo.hs
@@ -1,11 +1,39 @@
 module Todo
-  (
+  ( TodoList
+  , createOrder
   ) where
 
-data TodoList
-  = Leaf
-  | Branch
-  | Root
-  deriving (Show, Read, Eq)
+import Data.Maybe
+import Control.Monad.State
+import qualified Data.Set as Set
 
+data Date = Date deriving (Show, Read, Eq, Ord)
+
+data TodoList
+  = Leaf { title :: String, after :: [TodoList] }
+  | Branch
+      { title :: String
+      , description :: String
+      , dueDate :: Date
+      , after :: [TodoList]
+      }
+  | Root
+      { title :: String
+      , description :: String
+      , startDate :: Date
+      , endDate :: Date
+      , after :: [TodoList]
+      }
+  deriving (Show, Read, Eq, Ord)
+
+isLeaf :: TodoList -> Bool
+isLeaf (Leaf _ _) = True
+isLeaf _ = False
+
+isBranch :: TodoList -> Bool
+isBranch (Branch _ _ _ _) = True
+isBranch _ = False
+
+isRoot :: TodoList -> Bool
+isRoot x = not $ isLeaf x || isBranch x
 
