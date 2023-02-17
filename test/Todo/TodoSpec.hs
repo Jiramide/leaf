8df6347e39c37e3853d98270aa68bbd6a1af5ec7
Jade Piramide <jadepiramidepogi@gmail.com>
2023-02-16 17:11:31 -0800

    Insert a before hook to get the current time and change the currently existing tests to use the time to construct Branch and Root members.

--- test/Todo/TodoSpec.hs
+++ test/Todo/TodoSpec.hs
@@ -1,6 +1,7 @@
 module Todo.TodoSpec (spec) where
 
 import Test.Hspec
+import Data.Time (getZonedTime)
 import Todo.Todo
 
 isTopological :: [TodoItem] -> Bool
@@ -11,39 +12,40 @@ isTopological (x:xs) = locallyTopological && isTopological xs
 
 spec :: Spec
 spec = do
-  describe "shouldBeAfter" $ do
-    it "can properly determine that two TodoItems are direct dependents." $ do
-      (Leaf "after" [Leaf "before" []] `shouldBeAfter` Leaf "before" []) `shouldBe` True
-      (Branch "test" "desc" Date [Leaf "test" []] `shouldBeAfter` Leaf "test" []) `shouldBe` True
+  before getZonedTime $ do
+    describe "shouldBeAfter" $ do
+      it "can properly determine that two TodoItems are direct dependents." $ \time -> do
+        (Leaf "after" [Leaf "before" []] `shouldBeAfter` Leaf "before" []) `shouldBe` True
+        (Branch "test" "desc" time [Leaf "test" []] `shouldBeAfter` Leaf "test" []) `shouldBe` True
 
-    it "should properly determine that two TodoItems are indirectly dependent." $ do
-      (Branch
-        "Test"
-        "A branching branch"
-        Date
-        [ Leaf "Leafy leaves" [Leaf "Only child" []]
-        , Leaf "Uncle Leaf" []
-        ]
-        `shouldBeAfter`
-          Leaf "Only child" []
-        ) `shouldBe` True
+      it "should properly determine that two TodoItems are indirectly dependent." $ \time -> do
+        (Branch
+          "Test"
+          "A branching branch"
+          time
+          [ Leaf "Leafy leaves" [Leaf "Only child" []]
+          , Leaf "Uncle Leaf" []
+          ]
+          `shouldBeAfter`
+            Leaf "Only child" []
+          ) `shouldBe` True
 
-    it "should properly determine that two TodoItems are independent." $ do
-      (Leaf "Independent Leaf" [] `shouldBeAfter` Leaf "Independent Leafet" [])
-        `shouldBe` False
-      (Leaf "Independent Leafet" [Leaf "She's a single mother" []]
-        `shouldBeAfter` Leaf "Independent Leaf" [Leaf "He adopted an orphan leaf." []])
-        `shouldBe` False
+      it "should properly determine that two TodoItems are independent." $ \time -> do
+        (Leaf "Independent Leaf" [] `shouldBeAfter` Leaf "Independent Leafet" [])
+          `shouldBe` False
+        (Leaf "Independent Leafet" [Leaf "She's a single mother" []]
+          `shouldBeAfter` Leaf "Independent Leaf" [Leaf "He adopted an orphan leaf." []])
+          `shouldBe` False
 
-  describe "isLeaf" $ do
-    it "can correctly detect Leaf values." $ do
-      isLeaf (Leaf "test" []) `shouldBe` True
-      isLeaf (Leaf "not a leaf" [Leaf "leaf" []]) `shouldBe` True
+    describe "isLeaf" $ do
+      it "can correctly detect Leaf values." $ \time -> do
+        isLeaf (Leaf "test" []) `shouldBe` True
+        isLeaf (Leaf "not a leaf" [Leaf "leaf" []]) `shouldBe` True
 
-    it "can correctly disqualify Roots and Branches." $ do
-      isLeaf (Branch
-        "test test"
-        "I'm a branch constructed for testing!"
-        Date
-        [ Leaf "I'm a child!" [] ]
-        ) `shouldBe` False
+      it "can correctly disqualify Roots and Branches." $ \time -> do
+        isLeaf (Branch
+          "test test"
+          "I'm a branch constructed for testing!"
+          time
+          [ Leaf "I'm a child!" [] ]
+          ) `shouldBe` False
