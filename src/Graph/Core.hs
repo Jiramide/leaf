76c517495f8ebda3752ec566e9515755c413e40f
Jade Piramide <jadepiramidepogi@gmail.com>
2023-02-23 22:11:49 -0800

    Started on writing a Graph module, in order to allow other projects to use features like toposort. This module aims to have fast(-er) modification (insertion / deletion of vertices & edges) than the Data.Graph implementation, which is perfect for a dynamic application like todo item dependencies.

---
+++ src/Graph/Core.hs
@@ -0,0 +1,12 @@
+{-# LANGUAGE DeriveGeneric #-}
+
+module Graph.Core
+  ( Graph
+  ) where
+
+import GHC.Generics (Generic)
+import qualified Data.Map.Strict as Map
+
+newtype Graph c v
+  = Graph { graph :: Map.Map v (Map.Map v c) }
+  deriving (Eq, Show, Generic)
