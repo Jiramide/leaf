16a964cdc7b0359908776f016bbed33ff5551a89
Jade Piramide <jadepiramidepogi@gmail.com>
2023-02-28 18:55:22 -0800

    refactor: remove old code in Todo.Todo that interfaces with the old Todo data representation (createOrderN, createOrder, createOrderMany, shouldBeAfter). Topological sorting is intended to be in Graph.Graph, with Todo.TodoList providing a more appropriate interface for TodoItems.

--- src/Todo/Todo.hs
+++ src/Todo/Todo.hs
@@ -47,24 +47,3 @@ todoStartDate _ = Nothing
 todoEndDate :: TodoItem -> Maybe TodoTime
 todoEndDate (todoType -> Root _ _ end) = Just end
 todoEndDate _ = Nothing
-
-createOrderM :: TodoItem -> TodoList -> State (Set TodoItem) [TodoItem]
-createOrderM todo todoList = do
-  visited <- get
-  if (todo `member` visited)
-    then return []
-    else do
-      modify $ insert todo
-      subOrders <- forM (dependencies todo) createOrderM
-      return $ mconcat subOrders `mappend` [todo]
-
-createOrderMany :: TodoList -> TodoList
-createOrderMany = mconcat . (`evalState` empty) . mapM createOrderM
-
-createOrder :: TodoItem -> TodoList
-createOrder = (`evalState` empty) . createOrderM
-
-shouldBeAfter :: TodoItem -> TodoItem -> Bool
-shouldBeAfter todo before
-  = let dependents = after todo
-    in before `elem` dependents || any (`shouldBeAfter` before) dependents
